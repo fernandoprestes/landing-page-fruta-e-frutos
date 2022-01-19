74737adbd80e1fc0ebf67b40735710660c2e00c3
fernandoprestes <fernandoprestesc@gmail.com>
2022-01-19 18:39:59 -0300

    create banner section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Header } from './components/Header'
+import { Banner } from './modules/Banner'
+import { Revenue } from './modules/Revenue'
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Banner></Banner>
     </>
   )
 }
---
+++ src/modules/Banner/index.tsx
@@ -0,0 +1,9 @@
+import style from './banner.module.scss'
+
+export const Banner = () => {
+  return (
+    <section className={style.banner}>
+      <h2 className={style.banner__title}>Reaproveite melhor os alimentos</h2>
+    </section>
+  )
+}
